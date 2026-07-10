76f3ef8f8c9fb748a2b2a801034edc29d3e3099a
daniel-000com <danielorrala5552@gmail.com>
2026-07-09 21:51:46 -0500

    add from & to and jump buttons

--- tarjetas.js
+++ tarjetas.js
@@ -1,10 +1,24 @@
 function crearTarjetas(){
+    let desde = recuperarFloat("textDesde");
+    let hasta = recuperarFloat("textHasta");
+    let salto = recuperarFloat("textSalto");
     let contenido="";
     let divTarjetas;
     divTarjetas=document.getElementById("divTarjetas")
-    for(let i =0; i<=5; i ++){
+    for(let i =desde; i<=hasta; i +=salto){
         contenido = contenido + "<div class='item'>"+ i +"</div>"
         divTarjetas.innerHTML = contenido;
     }
      
+//reutilizar codigo ¶:v //
+}
+function recuperarTexto(idComponente){
+    let componente=document.getElementById(idComponente);
+    let valor =componente.value;
+    return valor; 
+}
+function recuperarFloat(idComponente){
+   let valorTexto=recuperarTexto(idComponente);
+   let valorFloat=parseFloat(valorTexto)
+   return valorFloat;
 }
